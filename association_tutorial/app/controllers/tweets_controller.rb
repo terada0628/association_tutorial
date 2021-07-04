44aefe0e67dd125ed3927bef64254144590bc029
terada0628 <ntpeanuts@yahoo.co.jp>
2021-07-04 12:27:29 +0000

    continued1

--- association_tutorial/app/controllers/tweets_controller.rb
+++ association_tutorial/app/controllers/tweets_controller.rb
@@ -7,6 +7,11 @@ class TweetsController < ApplicationController
   end
 
   def create
+    # userとtweetをアソシエーションさせた状態で保存
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+    @tweet.save
+    redirect_to tweets_path
   end
 
   def index
@@ -14,6 +19,13 @@ class TweetsController < ApplicationController
   end
 
   def show
-    @tweet = Tweet.find(params{:id})
+    @tweet = Tweet.find(params[:id])
+    @user = @tweet.user
   end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:body)  #tweetモデルのカラムのみを許可
+  end
+
 end
--- association_tutorial/db/schema.rb
+++ association_tutorial/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_04_103723) do
+ActiveRecord::Schema.define(version: 2021_07_04_111020) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

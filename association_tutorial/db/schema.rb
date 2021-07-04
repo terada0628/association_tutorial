8a36bb483acab93a26b1351d9fe8bc33de653f41
terada0628 <ntpeanuts@yahoo.co.jp>
2021-07-04 13:49:15 +0000

    continued2

--- association_tutorial/db/schema.rb
+++ association_tutorial/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_04_111020) do
+ActiveRecord::Schema.define(version: 2021_07_04_124004) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.text "body"
